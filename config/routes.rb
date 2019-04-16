96ef8056bb4bdd08377c1a3ece665fa649a83cae
huynhlv <huynhlevan1997@gmail.com>
2019-04-16 16:09:42 +0700

    Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   scope "(:locale)", locale: /en|vi/ do
-    
+    root "users#new"
+    resources :users
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190413054322) do
+ActiveRecord::Schema.define(version: 20190413091311) do
 
   create_table "banks", force: :cascade do |t|
     t.string "bank_name"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 20190413054322) do
     t.string "remember_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
