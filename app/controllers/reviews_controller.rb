22cf8551a6a50deb0021739b38ff568b9512ce26
Doan Hoang Vu <doanhuuanh4@gmail.com>
2019-04-23 16:00:55 +0700

    Edit, delete review - Edit template

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -35,9 +35,27 @@ class ReviewsController < ApplicationController
     redirect_to @review.tour
   end
 
+  def update
+    if @review.update_attributes review_params
+      flash[:success] = t "controllers.reviews.create.success"
+    else
+      flash[:danger] = t "controllers.reviews.create.danger"
+    end
+    redirect_to @review.tour
+  end
+
+  def destroy
+    if @review.destroy
+      flash[:success] = t "controllers.reviews.destroy.success"
+    else
+      flash[:danger] = t "controllers.reviews.destroy.danger"
+    end
+    redirect_to @review.tour
+  end
+
   private
 
-  def load_review
+  def set_review
     @review = Review.find_by id: params[:id]
     return if @review
     flash[:danger] = t "controllers.reviews.load.danger"
