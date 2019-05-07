95530b3c3dc4c290db144c05041af4482976c7d9
Doan Hoang Vu <doanhuuanh4@gmail.com>
2019-05-07 13:47:54 +0700

    Deloy

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -20,6 +20,7 @@ module UsersHelper
   end
 
   def check_liked review
-    review.likes.liked(current_user.id, review.id)
+    # review.likes.liked(current_user.id, review.id)
+    review.likes.find{|like| like.user_id == current_user.id}
   end
 end
